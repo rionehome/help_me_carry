ed6a18938429bbbcba4d7100b5bf14bc66a8e273
MiglyA <zq6295migly@gmail.com>
2019-06-16 15:40:10 +0900

    [update]10秒待機に変更

--- pkgs/hmc_start_node/scripts/start_node.py
+++ pkgs/hmc_start_node/scripts/start_node.py
@@ -4,8 +4,7 @@ import rospy
 from std_msgs.msg import String, Bool
 
 if __name__ == '__main__':
-
-	while True:
-		rospy.init_node("hmc_start_node")
-		pub = rospy.Publisher('follow_me_nlp/recognition_start', Bool, queue_size=10)
-		pub.publish(True)
+	rospy.sleep(10)
+	rospy.init_node("hmc_start_node")
+	pub = rospy.Publisher('follow_me_nlp/recognition_start', Bool, queue_size=10)
+	pub.publish(True)
