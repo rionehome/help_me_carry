722a369455d68fe9ba90e16142d7412399d809db
MiglyA <zq6295migly@gmail.com>
2019-06-16 13:57:59 +0900

    [add]start_node追加

---
+++ pkgs/hmc_start_node/scripts/start_node.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: UTF-8
+import rospy
+from std_msgs.msg import String, Bool
+
+if __name__ == '__main__':
+
+	while True:
+		rospy.init_node("hmc_start_node")
+		pub = rospy.Publisher('follow_me_nlp/recognition_start', Bool, queue_size=10)
+		pub.publish(True)
