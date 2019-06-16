440785d82d8aebb6170fa9151733c1a81405eabf
MiglyA <zq6295migly@gmail.com>
2019-06-17 02:33:53 +0900

    [update]sleepを削除

--- pkgs/hmc_start_node/scripts/start_node.py
+++ pkgs/hmc_start_node/scripts/start_node.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 # coding: UTF-8
 import rospy
-from std_msgs.msg import String, Bool
+from std_msgs.msg import Bool
 
 if __name__ == '__main__':
-	rospy.sleep(10)
 	rospy.init_node("hmc_start_node")
 	pub = rospy.Publisher('follow_me_nlp/recognition_start', Bool, queue_size=10)
 	pub.publish(True)
