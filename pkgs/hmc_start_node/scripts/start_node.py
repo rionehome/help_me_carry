d8a0402a503c5d8e45c3532ea0f294a2c829dc55
MiglyA <zq6295migly@gmail.com>
2019-06-17 09:49:22 +0900

    [fix]stat_nodeが正常に機能

--- pkgs/hmc_start_node/scripts/start_node.py
+++ pkgs/hmc_start_node/scripts/start_node.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 # coding: UTF-8
+import time
+
 import rospy
 from std_msgs.msg import Bool
 
 if __name__ == '__main__':
 	rospy.init_node("hmc_start_node")
+
 	pub = rospy.Publisher('follow_me_nlp/recognition_start', Bool, queue_size=10)
+	time.sleep(1)
 	pub.publish(True)
