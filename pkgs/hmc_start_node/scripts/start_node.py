ac18f6363a28531a3024e481b1726d1598bbbfbe
MiglyA <zq6295migly@gmail.com>
2019-06-19 05:23:25 +0900

    [fix]メッセージ名のミスを修正

--- pkgs/hmc_start_node/scripts/start_node.py
+++ pkgs/hmc_start_node/scripts/start_node.py
@@ -8,6 +8,6 @@ from std_msgs.msg import Bool
 if __name__ == '__main__':
 	rospy.init_node("hmc_start_node")
 
-	pub = rospy.Publisher('follow_me_nlp/recognition_start', Bool, queue_size=10)
+	pub = rospy.Publisher('hmc_follow_me_nlp/recognition_start', Bool, queue_size=10)
 	time.sleep(1)
 	pub.publish(True)
