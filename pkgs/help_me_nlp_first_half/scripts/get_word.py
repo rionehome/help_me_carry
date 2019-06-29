cfef5a30ebc21df733b1cab9b30bc7b9eb08a760
MiglyA <zq6295migly@gmail.com>
2019-06-30 00:12:01 +0900

    [fix]記述ミスを修正

--- pkgs/help_me_nlp_first_half/scripts/get_word.py
+++ pkgs/help_me_nlp_first_half/scripts/get_word.py
@@ -6,17 +6,19 @@ import os
 
 def get_Index_of_nextNoun(sentence, word, result):
     index_of_word = sentence.index(word)
-    for k in range(index_of_word+1, len(sentence)):
+    for k in range(index_of_word + 1, len(sentence)):
         if (result[k][1].startswith('N')):
             return k
-    for k in range(index_of_word+1, len(sentence)):
-        if  (result[k][1].startswith('PP')):
+    for k in range(index_of_word + 1, len(sentence)):
+        if (result[k][1].startswith('PP')):
             return k
+
 
 def select_Verb(sentence, word_list):
     for verb in word_list:
         if verb in sentence:
-            return verb   #動詞を返す
+            return verb  # 動詞を返す
+
 
 def carry_func(sentence, verb, result):
     ans = []
@@ -34,52 +36,55 @@ def carry_func(sentence, verb, result):
         ans.append(place)
         
         index_of_next = get_Index_of_nextNoun(sentence, place, result)
-        if index_of_next!=True:
+        if index_of_next != True:
             return ans
         thing = result[index_of_next][0]
         ans.append(thing)
         return ans
 
+
 def cut_sentence(result):
     verb_count = []
-    txt_list = []   
+    txt_list = []
     for num, word_class in enumerate(result):
         if word_class[1].startswith('VV'):
-            verb_count.append(num)#動詞の格納されているインデックスを獲得
-
+            verb_count.append(num)  # 動詞の格納されているインデックスを獲得
+    
     for num in range(len(verb_count)):
-        if num+1 == len(verb_count):
+        if num + 1 == len(verb_count):
             txt_list.append(result[verb_count[num]:])
             break
-        txt_list.append(result[verb_count[num]:verb_count[num+1]])
-        
+        txt_list.append(result[verb_count[num]:verb_count[num + 1]])
+    
     return txt_list
 
+
 def main(txt):
-    TTWDIR=os.environ['HOME']+'/tree-tagger-install/'
-    tagger=ttw.TreeTagger(TAGLANG='en', TAGDIR=TTWDIR)
-
-    carry=['carry', 'accompany', 'escort', 'deliver', 'bring', 'place', 'navigate', 'locate', 'send', 'bear', 'channel', 'transport', 'transmit', 'submit', 'take']
+    TTWDIR = os.environ['HOME'] + '/tree-tagger-install/'
+    tagger = ttw.TreeTagger(TAGLANG='en', TAGDIR=TTWDIR)
     
-    sentence = (txt).decode('utf-8')#treetagger用にutf-8へ
-
+    carry = ['carry', 'accompany', 'escort', 'deliver', 'bring', 'place', 'navigate', 'locate', 'send', 'bear',
+             'channel', 'transport', 'transmit', 'submit', 'take']
+    
+    sentence = (txt).decode('utf-8')  # treetagger用にutf-8へ
+    
     result = []
     answer = []
     tags = tagger.TagText(sentence)
-
+    
     for i in tags:
         li = i.split()
-        result.append(li[:2]) #('動詞', '品詞')
-
+        result.append(li[:2])  # ('動詞', '品詞')
+    
     result_list = cut_sentence(result)
-
+    
     sentence = ''
     result = result_list.pop(0)
     
     for word in result:
-        sentence += word[0]+' '
+        sentence += word[0] + ' '
     sentence = sentence.split()
-    verb = select_Verb(sentence, carry) #文中の動詞を獲得
+    verb = select_Verb(sentence, carry)  # 文中の動詞を獲得
     if verb in carry:
         answer = carry_func(sentence, verb, result)
     return answer
