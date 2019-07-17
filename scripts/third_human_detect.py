25181d2b69a39a2ff969814006a0ab532fdae778
Nenetti <akinori1090@gmail.com>
2019-07-17 13:52:52 +0900

    [add] 人間検出部分のファイルを追加、ただし中身はまだ

---
+++ scripts/third_human_detect.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Follow me, 自然言語処理
+
+import rospy
+from sound_system.srv import *
+from std_msgs.msg import String, Bool, Int32
+from location.srv import RegisterLocation, RequestLocation
+from location.msg import Location
+from hmc_start_node.msg import Activate
+
+
+class ThirdHumanDetect:
+
+    def __init__(self):
+        self.activate_no = 3
+
+        rospy.init_node("hmc_human_detect", anonymous=True)
+
+        self.activate_pub = rospy.Publisher("/help_me_carry/activate", Activate, queue_size=10)
+
+    ########################################################
+    #       この main関数内が一連の処理の流れとなる        #
+    ########################################################
+    def main(self):
+        # 現状、人間検出の処理がどうなるか不明なので一旦保留
+        self.next_node_activate()
+
+    ########################################################
+    #               それぞれの分岐する処理                 #
+    ########################################################
+
+    ########################################################
+    #               Activate関係の処理                     #
+    ########################################################
+    def activate_callback(self, message):
+        # type: (Activate) -> None
+        """
+        指定IDがこのノードのIDなら処理開始
+        :param message: Activateメッセージ
+        :return: なし
+        """
+        if self.activate_no == message.id:
+            self.main()
+
+    def next_node_activate(self):
+        # type: () -> None
+        """
+        次のノード(現在のID+1)に起動コマンドを送る
+        :return: なし
+        """
+        activate = Activate()
+        activate.id = self.activate_no + 1
+        self.activate_pub.publish(activate)
+
+    ########################################################
+    #         ここからは、ちょっとした細かい処理の関数群         #
+    ########################################################
+
+
+if __name__ == "__main__":
+    ThirdHumanDetect()
+    rospy.spin()
